09aab6d0e6fe0c369a0feaec6cc3ae7172a0a631
mQxz <>
2023-03-04 16:18:04 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("super man!"));
+app.get("/", (req, res) => res.send("hello Shane Qiao!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
